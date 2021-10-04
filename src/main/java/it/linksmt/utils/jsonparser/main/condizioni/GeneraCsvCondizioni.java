41c9b239b00b17db0ea3ba357c1c274625338cf4
serraan <xveeshax@gmail.com>
2021-10-04 10:52:47 +0200

    Fix output PATH

--- src/main/java/it/linksmt/utils/jsonparser/main/condizioni/GeneraCsvCondizioni.java
+++ src/main/java/it/linksmt/utils/jsonparser/main/condizioni/GeneraCsvCondizioni.java
@@ -17,7 +17,7 @@ public class GeneraCsvCondizioni {
 
     static final String[] HEADERS = {"aggregato", "condizione", "attr1_nome", "attr1_valore", "attr2_nome", "attr2_valore"};
     // CAMBIARE QUESTO PATH CON QUELLO NEL QUALE SI VUOLE SALVARE IL FILE.
-    static final String PATH = new JFileChooser().getFileSystemView().getDefaultDirectory().toString();
+    static final String PATH = new JFileChooser().getFileSystemView().getDefaultDirectory().toString() + "\\output.csv";
     static final String RESOURCE_NAME = "condizioni.json";
 
     public static void main(String[] args) throws IOException {
