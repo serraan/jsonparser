62dc1a178c64fbd48700c8c28337d2b910c45eb4
serraan <xveeshax@gmail.com>
2021-10-04 12:26:36 +0200

    Aggiunta possibilità di input da console

--- src/main/java/it/linksmt/utils/jsonparser/main/condizioni/GeneraCsvCondizioni.java
+++ src/main/java/it/linksmt/utils/jsonparser/main/condizioni/GeneraCsvCondizioni.java
@@ -2,54 +2,77 @@ package it.linksmt.utils.jsonparser.main.condizioni;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.linksmt.utils.jsonparser.main.utils.Utils;
+import it.linksmt.utils.jsonparser.types.Condizione;
 import it.linksmt.utils.jsonparser.types.TopLevel;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 
 import javax.swing.*;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GeneraCsvCondizioni {
 
     static final String[] HEADERS = {"aggregato", "condizione", "attr1_nome", "attr1_valore", "attr2_nome", "attr2_valore"};
-    // CAMBIARE QUESTO PATH CON QUELLO NEL QUALE SI VUOLE SALVARE IL FILE.
-    static final String PATH = new JFileChooser().getFileSystemView().getDefaultDirectory().toString() + "\\output.csv";
-    static final String RESOURCE_NAME = "condizioni.json";
 
-    public static void main(String[] args) throws IOException {
-        Utils utils = new Utils();
-        ObjectMapper objectMapper = new ObjectMapper();
-        String moneticaJson = utils.getJsonStringFromFile(RESOURCE_NAME);
-        TopLevel monetica = objectMapper.readValue(moneticaJson, TopLevel.class);
-
-        List<List<String>> lines = new ArrayList<>();
-
-        monetica.getDecorrenze().get(0).getAggregati().stream().forEach(aggregato -> {
-            aggregato.getCondizioni().stream().forEach(condizione -> {
-                List<String> values = new ArrayList<>();
-                values.add(aggregato.getNome());
-                values.add(condizione.getCodice());
-                values.add(condizione.getAttributi().get(0).getNome());
-                values.add(condizione.getAttributi().get(0).getValore());
-                values.add(condizione.getAttributi().get(1).getNome());
-                values.add(condizione.getAttributi().get(1).getValore());
-                lines.add(values);
-            });
-        });
-
-        File outputPath = new File(PATH);
-
-        FileWriter out = new FileWriter(outputPath);
-        try (CSVPrinter printer = new CSVPrinter(out, CSVFormat.EXCEL
-                .withHeader(HEADERS)
-                .withDelimiter('\t'))) {
-            printer.printRecords(lines);
+    public static void main(String[] args) {
+        try {
+            if (args.length > 0) { // Il path è stato passato da command line
+                generateCsv(args[0].trim());
+            } else { // Il path viene richiesto in console
+                generateCsv(readFromConsole());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void generateCsv(String resourcePath) throws IOException {
+        if (resourcePath.endsWith(".json")) {
+            Utils utils = new Utils();
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            String decorrenzeJson = utils.readJsonStringFromFile(resourcePath);
+            TopLevel monetica = objectMapper.readValue(decorrenzeJson, TopLevel.class);
+
+            List<List<String>> lines = new ArrayList<>();
+
+            monetica.getDecorrenze().get(0).getAggregati().forEach(aggregato -> {
+                aggregato.getCondizioni().forEach(condizione -> {
+                    List<String> values = new ArrayList<>();
+                    values.add(aggregato.getNome());
+                    values.add(condizione.getCodice());
+                    values.add(condizione.getAttributi().get(0).getNome());
+                    values.add(condizione.getAttributi().get(0).getValore());
+                    values.add(condizione.getAttributi().get(1).getNome());
+                    values.add(condizione.getAttributi().get(1).getValore());
+                    lines.add(values);
+                });
+            });
+
+            String outPath = resourcePath.replace(".json", ".csv");
+            File outputPath = new File(outPath);
+
+            FileWriter out = new FileWriter(outputPath);
+            try (CSVPrinter printer = new CSVPrinter(out, CSVFormat.EXCEL
+                    .withHeader(HEADERS)
+                    .withDelimiter('\t'))) {
+                printer.printRecords(lines);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("CSV generato correttamente in: " + outputPath);
+        } else {
+            System.out.print("Il file passato in input non è un file json, riprovare.");
+        }
+        System.out.println("Premi invio per uscire...");
+        System.in.read();
+    }
+
+    private static String readFromConsole() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Inserisci il path del json delle decorrenze:");
+        return br.readLine().trim();
     }
 }
--- src/main/java/it/linksmt/utils/jsonparser/main/utils/Utils.java
+++ src/main/java/it/linksmt/utils/jsonparser/main/utils/Utils.java
@@ -2,16 +2,20 @@ package it.linksmt.utils.jsonparser.main.utils;
 
 import org.apache.commons.io.FileUtils;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class Utils {
 
-	public String getJsonStringFromFile(String json) throws IOException {
-		ClassLoader classLoader = getClass().getClassLoader();
-		File file = new File(classLoader.getResource(json).getFile());
-		String out = FileUtils.readFileToString(file, "UTF-8");
-		return out;
+	public String readJsonStringFromFile(String jsonPath) throws IOException {
+		Path path = Paths.get(jsonPath);
+		return new String(Files.readAllBytes(path));
 	}
 
 }
